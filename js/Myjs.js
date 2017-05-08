75fda32adc19625d4065534c17b1f92a65ac3605
Brian Spray <Evenidontkno@live.com>
2017-05-07 21:08:18 -0400

    Holy mother of god we got a thing

--- js/Myjs.js
+++ js/Myjs.js
@@ -136,42 +136,79 @@ map.on('draw:created', function (e) {
 });
 
 
-var margin =  {
-    top: 20,
-    right: 20,
-    bottom: 30,
-    left: 40
-}, 
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
 
-var x = d3.scaleBand().range([0, width]).padding(0.1);
-
-var y = d3.scaleLinear().range([height, 0]);
-
-var svg = d3.select("#graph-display").append("svg").attr("width", width + margin.left + margin.right).attr("height", height + margin.top + margin.bottom).append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 function barChart(e) {
 
-    x.domain(e.map(function(d) { return d.tripid; }));
-    y.domain([0, d3.max(e, function(d) { return d.avspeed; })]);
+    var margin =  {
+        top: 20,
+        right: 20,
+        bottom: 50,
+    left: 40
+    }, 
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+
+    var x = d3.scaleBand().rangeRound([0, width], .05).padding(0.1);
+
+    var y = d3.scaleLinear().range([height, 0]);
+
+    var xAxis = d3.axisBottom().scale(x).tickFormat(d3.timeFormat("%I:%M %p"));
+
+    var yAxis = d3.axisLeft().scale(y).ticks(10);
+
+    var svg = d3.select("#graph-display").append("svg")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+                .append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    var hourParser = d3.isoParse; //d3.timeParse("%I:%M%p");
+
+    /*e.forEach(function(d) {
+        d.date = d3.timeHour.round(hourParser(d.starttime));
+        d.avspeed = +d.avspeed;
+        console.log("Start Time: " + timeFormatter(d3.timeHour.round(d.date)) + " AvSpeed: " + d.avspeed);
+    });*/
+
+    var dataset = d3.nest()
+        .key(function (d) { return d3.timeHour.round(hourParser(d.starttime)); }).sortKeys(d3.ascending)
+        .rollup(function (d) { return d3.mean(d, function (g) { return g.avspeed; }); })
+        .entries(e);
+        console.log(JSON.stringify(dataset));
+
+
+    x.domain(dataset.map(function(d) { return new Date(d.key); }));
+    y.domain([0, d3.max(dataset, function(d) { return d.value; })]);
 
    svg.selectAll(".bar")
-    .data(e)
+    .data(dataset)
    .enter().append("rect")
     .attr("class", "bar")
-    .attr("x", function(d) { return d.tripid; })
+    .attr("x", function(d) { return x(new Date(d.key)); })
     .attr("width", x.bandwidth())
-    .attr("y", function(d) { return d.avspeed; })
-    .attr("height", function(d) { return height - y(d.avspeed); });
+    .attr("y", function(d) { return y(d.value); })
+    .attr("height", function(d) { return height - y(d.value); });
 
    svg.append("g")
       .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
+      .call(xAxis)
+      .selectAll("text")
+      .style("text-anchor", "end")
+      .attr("dx", "-.8em")
+      .attr("dy", "-.55em")
+      .attr("transform", "rotate(-45)" );
 
   // add the y Axis
-  svg.append("g")
-      .call(d3.axisLeft(y));
+    svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Value");
 
 }
 
